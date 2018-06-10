ce7bb89a87c7880af72af9750dcdce8c92e544cc
Jeff Polakiewicz <shaangor@gmail.com>
2018-06-10 18:06:18 -0400

    Creating db user when logging in.

--- src/firebase/firebase.config.ts
+++ src/firebase/firebase.config.ts
@@ -1,38 +1,54 @@
 import * as firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
 import * as firebaseui from 'firebaseui';
 import { store } from 'src';
 import { loginSuccess } from 'src/auth/auth.actions';
 
-const config = {
+export const firebaseApp = firebase.initializeApp({
     apiKey: "AIzaSyDWMGvNAvl10PX6nP0Fzar6Jtv1g6RNlyk",
     authDomain: "board-game-night-planner.firebaseapp.com",
     databaseURL: "https://board-game-night-planner.firebaseio.com",
     messagingSenderId: "622458243132",
     projectId: "board-game-night-planner",
     storageBucket: "board-game-night-planner.appspot.com",
-};
+});
 
-export const firebaseApp = firebase.initializeApp(config);
+const auth = firebaseApp.auth();
+const db = firebase.firestore();
+db.settings({
+    timestampsInSnapshots: true,
+});
 
-const ui = new firebaseui.auth.AuthUI(firebaseApp.auth());
-const uiConfig: firebaseui.IConfig = {
-    callbacks: {
-        signInSuccessWithAuthResult: (authResult, redirectUrl) => {
-            return false;
-        }
-    },
-    signInFlow: 'popup',
-    signInOptions: [
-        firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-    ],
-};
+enum Collections {
+    Users = "users",
+}
 
-export function firebaseUiAuthStart(elementId: string): void {
-    ui.start(elementId, uiConfig);
-};
-
-firebaseApp.auth().onAuthStateChanged(user => {
+auth.onAuthStateChanged(user => {
     if (user) {
         store.dispatch(loginSuccess(user.uid));
     }
 });
+
+const ui = new firebaseui.auth.AuthUI(auth);
+
+export function firebaseUiAuthStart(elementId: string): void {
+    ui.start(elementId, {
+        callbacks: {
+            signInSuccessWithAuthResult: (authResult, redirectUrl) => {
+                if (authResult.user) {
+                    db.collection(Collections.Users).doc(authResult.user.uid).set({
+                        displayName: authResult.user.displayName,
+                        email: authResult.user.email,
+                        isAdmin: false,
+                    });
+                }
+                return false;
+            }
+        },
+        signInFlow: 'popup',
+        signInOptions: [
+            firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+        ],
+    });
+};
