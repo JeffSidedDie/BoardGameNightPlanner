f2837ed8de6f15b7c3e3eaeadd41b697e8de035f
Jeff Polakiewicz <shaangor@gmail.com>
2019-02-27 16:06:08 -0500

    Trying react-datetime for datetime picker component.

---
+++ src/common/components/datetime-field.tsx
@@ -0,0 +1,38 @@
+import 'react-datetime/css/react-datetime.css';
+
+import * as Formik from 'formik';
+import { Moment } from 'moment';
+import * as React from 'react';
+import * as Datetime from 'react-datetime';
+import { Omit } from './omit';
+
+type DateTimeFieldProperties<T> = Formik.FieldProps<T> & Omit<React.InputHTMLAttributes<HTMLInputElement>, 'form'>;
+
+export class DateTimeField<T> extends React.Component<DateTimeFieldProperties<T>> {
+
+    private datetimeRef: React.RefObject<Datetime> = React.createRef<Datetime>();
+
+    // public componentDidUpdate(prevProps: Readonly<DateTimeFieldProperties<T>>) {
+    //     if (prevProps.field.value && !this.props.field.value && this.datetimeRef.current && this.datetimeRef.current.props.onChange) {
+
+    //     }
+    // }
+
+    public render() {
+        return <Datetime
+            ref={this.datetimeRef}
+            value={this.props.field.value}
+            onChange={this.handleFileInputChange}
+            inputProps={{
+                className: 'input',
+                id: this.props.field.name,
+                name: this.props.field.name,
+            }}
+        />;
+        // return <input ref={this.inputRef} id={this.props.field.name} name={this.props.field.name} type={this.props.type} onChange={this.handleFileInputChange} />;
+    }
+
+    private handleFileInputChange = (value: Moment | string) => {
+        this.props.form.setFieldValue(this.props.field.name, value);
+    }
+}
--- src/events/event-form.component.tsx
+++ src/events/event-form.component.tsx
@@ -1,6 +1,7 @@
 import * as firebase from 'firebase';
 import { Form, Formik, FormikActions, FormikProps } from 'formik';
 import * as React from 'react';
+import { DateTimeField } from 'src/common/components/datetime-field';
 import { Field } from 'src/common/components/field';
 import { GamesSelect } from 'src/common/components/games-select.container';
 import { EventData, EventDocument, GameDocument } from 'src/common/models';
@@ -48,22 +49,11 @@ export class EventFormComponent extends React.Component<EventFormComponentProper
     private renderForm = (props: FormikProps<EventData>) => {
         return <Form>
             <h2 className="title">Edit Event</h2>
-            <Field<EventData> name="timestamp" label="Timestamp" type="date" />
+            <Field<EventData> name="timestamp" label="Timestamp" component={DateTimeField} />
             <Field<EventData> name="game" label="Game" type="file" component={GamesSelect} />
             <button className="button is-primary" type="submit">Save</button>
         </Form>;
     }
-
-    // private renderGamesSelect = (props: FieldProps<EventData>) => {
-    //     return <ObjectSelectField<EventData, GameDocument>
-    //         values={this.props.games}
-    //         keySelector={this.gameKeySelector}
-    //         labelSelector={this.gameLabelSelector}
-    //         {...props} />;
-    // }
-
-    // private gameKeySelector = (game: GameDocument) => game.id;
-    // private gameLabelSelector = (game: GameDocument) => game.data.name;
 
     private handleSubmit = async (values: EventData, formikActions: FormikActions<EventData>) => {
         await this.props.saveEvent(values, this.props.event ? this.props.event.id : '');
