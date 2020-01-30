d24de42c53c53fa5fa92e3783f4a751744db190b
Jeff Polakiewicz <jeff.polakiewicz@imaginexconsulting.com>
2020-01-30 17:38:55 -0500

    Converted GeneratedLink to class to override shouldComponentUpdate.

--- src/common/components/generatedLink.tsx
+++ src/common/components/generatedLink.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
 import { Link } from 'react-router-dom';
 import { generateRoute, Routes } from 'common/routes';
 
@@ -6,10 +6,30 @@ export interface GeneratedLinkProperties {
     readonly route: Routes;
     readonly parameters?: { [property: string]: string | number | undefined };
     readonly className?: string;
+    readonly children?: ReactNode;
 }
 
-export const GeneratedLink: React.FC<GeneratedLinkProperties> = (props) => {
-    const route = generateRoute(props.route, props.parameters);
-    return <Link className={props.className} to={route}>{props.children}</Link>;
+export class GeneratedLink extends React.Component<GeneratedLinkProperties> {
+    static whyDidYouRender = true;
+
+    private generatedRoute: string = '';
+
+    constructor(props: Readonly<GeneratedLinkProperties>) {
+        super(props);
+        this.generatedRoute = generateRoute(props.route, props.parameters);
+    }
+
+    shouldComponentUpdate(nextProps: GeneratedLinkProperties) {
+        const newRoute = generateRoute(nextProps.route, nextProps.parameters);
+        if (this.generatedRoute !== newRoute) {
+            this.generatedRoute = newRoute;
+            return true;
+        }
+        return this.props.className !== nextProps.className
+            || this.props.children !== nextProps.children;
+    }
+
+    public render() {
+        return <Link className={this.props.className} to={this.generatedRoute}>{this.props.children}</Link>;
+    }
 }
-GeneratedLink.whyDidYouRender = true;
